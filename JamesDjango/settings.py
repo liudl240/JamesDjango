94e94c7eea1cc2dc73baa2f05c0fcbeeff077f9a
root <root@django.james.com>
2019-05-22 01:47:24 -0400

    主页

--- JamesDjango/settings.py
+++ JamesDjango/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'websites',
-    'sites',
+    'mysites',
+    'Users',
 ]
 
 MIDDLEWARE = [
@@ -129,5 +129,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, "static"),
+]
