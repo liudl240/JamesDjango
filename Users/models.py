2257c9909a831b4f9b8cbf3fe584bfae56c05705
ybooks.liu <ybooks240@gmail.com>
2019-06-09 19:22:16 +0800

    优化用户信息

--- Users/models.py
+++ Users/models.py
@@ -3,13 +3,16 @@ from django.db import models
 class UserInfo(models.Model):
     '''用户表'''
     username = models.CharField(max_length=128,unique=True)
-    nickname = models.CharField(max_length=128,null=True)
-    remark = models.CharField(max_length=244,null=True)
+    nickname = models.CharField(max_length=128,verbose_name="昵称", default="")
+    cellphone = models.CharField(max_length=11, verbose_name="手机号码", default="")
+    remark = models.CharField(max_length=244, verbose_name="简介备注",  default="")
     password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
     active = models.BooleanField(default='0')
     c_time = models.DateTimeField(null=False)
     l_time = models.DateTimeField(null=True)
+    Avatar = models.ImageField(upload_to='Avatar',unique=False,default="/Avatar/cat.jpg")
+    Avatar_name = models.CharField(max_length=128,default="cat.jpg")
     def __str__(self):
         return self.name
     class Meta:
